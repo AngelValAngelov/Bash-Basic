034f94cbc3b620761b1ba3c244c5e754fc7a01f0
Angel Angelov <64867029+AngelValAngelov@users.noreply.github.com>
2024-01-26 01:08:40 +0200

    Add files via upload

---
+++ Basics/exprtest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+num1=10
+num2=5
+
+echo "$num1 - $num2 = $(expr $num1 - $num2)"
+echo "$num1 + $num2 = $(expr $num1 + $num2)"
+echo "$num1 / $num2 = $(expr $num1 / $num2)"
+echo "$num1 * $num2 = $(expr $num1 \* $num2)"
+
